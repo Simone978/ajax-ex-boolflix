0a9735cc06ac1d633668735ae2b21188696042d2
Simone <simonelega.sl@gmail.com>
2020-02-06 14:30:24 +0100

    Commit iniziale

---
+++ js/script.js
@@ -0,0 +1,8 @@
+// Creare un layout base con una searchbar (una input e un button) in cui possiamo scrivere completamente o parzialmente il nome di un film. Possiamo, cliccando il  bottone, cercare sull’API tutti i film che contengono ciò che ha scritto l’utente.
+// Vogliamo dopo la risposta dell’API visualizzare a schermo i seguenti valori per ogni film trovato:
+// Titolo
+// Titolo Originale
+// Lingua
+// Voto
+
+// 07e3257a7ad00294a8c003683909f65c
