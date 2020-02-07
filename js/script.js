11af2141ee8fce714412c526f663139f081b246a
Simone <simonelega.sl@gmail.com>
2020-02-07 13:55:10 +0100

    trasformo il valore dei voti in numeri interi

--- js/script.js
+++ js/script.js
@@ -5,42 +5,59 @@
 // Lingua
 // Voto
 
+// Trasformiamo il voto da 1 a 10 decimale in un numero intero da 1 a 5, così da permetterci di stampare a schermo un numero di stelle piene che vanno da 1 a 5, lasciando le restanti vuote (troviamo le icone in FontAwesome).
+// Arrotondiamo sempre per eccesso all’unità successiva, non gestiamo icone mezze piene
+
 // 07e3257a7ad00294a8c003683909f65c
 //https://api.themoviedb.org/3/search/movie
 
 $(document).ready(function(){
   $('.search').click(function(){
-    $('.movie_list').html('');
+
+    reset_list();
     var titolo = $("#string").val();
     console.log(titolo);
     // chiamata ajax
-    $.ajax(
-      {
-      url: "https://api.themoviedb.org/3/search/movie",
-      method: "GET",
-      data: {
-            api_key: "07e3257a7ad00294a8c003683909f65c",
-            query: titolo,
-            language: "it"
-          },
-      success: function (data) {
-        var dati = data.results;
-        if(!dati.length==0){
-        print(dati);
-      }else{
-        alert('questa ricerca non ha prodotto risultati');
-      }
-        },
-      error: function (richiesta, stato, errori) {
-        alert("E' avvenuto un errore. " + errori);
-        }
-      }
-    );
+    movieData(titolo);
 
   });
 });
 
 // Funzioni
+
+function movieData(string){
+  $.ajax(
+    {
+    url: "https://api.themoviedb.org/3/search/movie",
+    method: "GET",
+    data: {
+          api_key: "07e3257a7ad00294a8c003683909f65c",
+          query: string,
+          language: "it"
+        },
+    success: function (data) {
+      var dati = data.results;
+      if(!dati.length==0){
+      print(dati);
+    }else{
+      alert('questa ricerca non ha prodotto risultati');
+    }
+      },
+    error: function (richiesta, stato, errori) {
+      alert("E' avvenuto un errore. " + errori);
+      }
+    }
+  );
+  resetString();
+};
+
+function reset_list(){
+  $('.movie_list').html('');
+}
+
+function resetString(){
+  $('#string').val('');
+};
 
 function print(dati){
   var source = $('#movies_data').html();
@@ -49,6 +66,8 @@ function print(dati){
   for (var i = 0; i < dati.length; i++) {
     var film = dati[i];
     var context = film;
+    var voti = Math.ceil(film.vote_average);
+    console.log(voti);
     // var context = {
     //    title: film.title,
     //    original_title: film.original_title,
