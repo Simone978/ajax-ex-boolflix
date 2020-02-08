c10c55738e77deeec8bdf04777ec23d799728abb
Simone <simonelega.sl@gmail.com>
2020-02-08 17:57:01 +0100

    effettuo nuova chiamata per serie tv e riadatto handlebars

--- js/script.js
+++ js/script.js
@@ -10,6 +10,10 @@
 
 // Trasformiamo poi la stringa statica della lingua in una vera e propria bandiera della nazione corrispondente, gestendo il caso in cui non abbiamo la bandiera della nazione ritornata dall’API (le flag non ci sono in FontAwesome).
 
+// Allarghiamo poi la ricerca anche alle serie tv. Con la stessa azione di ricerca dovremo prendere sia i film che corrispondono alla query, sia le serie tv, stando attenti ad avere alla fine dei valori simili (le serie e i film hanno campi nel JSON di risposta diversi, simili ma non sempre identici)
+// Qui un esempio di chiamata per le serie tv:
+// https://api.themoviedb.org/3/search/tv?api_key=e99307154c6dfb0b4750f6603256716d&language=it_IT&query=scrubs
+
 // 07e3257a7ad00294a8c003683909f65c
 //https://api.themoviedb.org/3/search/movie
 
@@ -22,6 +26,7 @@ $(document).ready(function(){
     console.log(titolo);
     // chiamata ajax
     movieData(titolo);
+    serieTvData(titolo);
 
   });
 
@@ -40,6 +45,33 @@ function movieData(string){
           language: "it"
         },
     success: function (data) {
+      var dati = data.results;
+      if(!dati.length==0){
+      print(dati);
+    }else{
+      alert('questa ricerca non ha prodotto risultati');
+    }
+      },
+    error: function (richiesta, stato, errori) {
+      alert("E' avvenuto un errore. " + errori);
+      }
+    }
+  );
+  resetString();
+};
+
+function serieTvData(string){
+  $.ajax(
+    {
+    url: "https://api.themoviedb.org/3/search/tv",
+    method: "GET",
+    data: {
+          api_key: "07e3257a7ad00294a8c003683909f65c",
+          query: string,
+          language: "it"
+        },
+    success: function (data) {
+      console.log(data);
       var dati = data.results;
       if(!dati.length==0){
       print(dati);
@@ -84,21 +116,22 @@ function print(dati){
   for (var j = 0; j < dati.length; j++) {
     var film = dati[j];
     var context = film;
+    var title = film.original_name;
     var votazione = film.vote_average / 2;
     var voti = Math.ceil(votazione);
-    console.log(voti);
+    console.log(film.original_name);
     var flag = film.original_language;
     if (flag != "it" && flag != "en" && flag != "fr") {
       flag = "";
     }
 
     var context = {
-       title: film.title,
-       original_title: film.original_title,
-       original_language: film.original_language,
-       flag: flag,
-       vote_average: voti,
-       stars: printStars(voti)
+      original_title: film.original_title,
+      original_name: film.original_name,
+      original_language: film.original_language,
+      flag: flag,
+      vote_average: voti,
+      stars: printStars(voti)
      };
      var html = template(context);
      $('.movie_list').append(html);
